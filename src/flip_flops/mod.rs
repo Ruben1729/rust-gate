a1986e8d6e577e5afedfc51b0d3877c9141d28a6
Ruben1729 <ruben.e.sanchez.a@gmail.com>
2024-06-02 21:58:12 -0400

    Flipflop

--- src/flip_flops/mod.rs
+++ src/flip_flops/mod.rs
@@ -1,2 +1,48 @@
+use crate::error::LatchError;
+use crate::logic_gate::*;
+use crate::latch::*;
+use crate::SignalEdge;
 
+trait FlipFlop {
+    fn set(&mut self, a: bool, b: bool, clk: bool) -> Result<(), LatchError>;
+    fn set_n(&mut self, na: bool, nb: bool, clk: bool) -> Result<(), LatchError>;
+    fn evaluate(&mut self);
+    fn get_q(&self) -> bool;
+    fn get_nq(&self) -> bool;
+}
 
+#[derive(Debug)]
+pub struct SrFlipFlop {
+    clk: bool,
+    sr_latch: SrLatch,
+    edge_trigger: SignalEdge
+}
+
+impl FlipFlop for SrFlipFlop {
+    fn set(&mut self, s: bool, r: bool, clk: bool) -> Result<(), LatchError> {
+        let did_change = XorGate::evaluate(clk, self.clk);
+
+        match self.edge_trigger {
+            SignalEdge::Rising if did_change && !clk => self.sr_latch.set(s, r),
+            SignalEdge::Falling if did_change && clk => self.sr_latch.set(s, r),
+            SignalEdge::Change => self.sr_latch.set(s, r),
+            _ => Ok(())
+        }
+    }
+
+    fn set_n(&mut self, na: bool, nb: bool, clk: bool) -> Result<(), LatchError> {
+        self.set(NotGate::evaluate(na), NotGate::evaluate(nb), clk)
+    }
+
+    fn evaluate(&mut self) {
+        self.sr_latch.evaluate()
+    }
+
+    fn get_q(&self) -> bool {
+        self.sr_latch.get_q()
+    }
+
+    fn get_nq(&self) -> bool {
+        self.sr_latch.get_nq()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod flip_flops;
 mod latch;
 mod logic_gate;
+mod signal;
 
 pub use self::flip_flops::*;
 pub use self::latch::*;
 pub use self::logic_gate::*;
+pub use self::signal::*;
